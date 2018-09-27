d8fd4d7dfa8ad7fc8a465b9bd24cab72ebdd3d4d
mleung07 <mleung_07@hotmail.com>
2018-09-27 21:24:20 +0800

    add pwa

---
+++ precache-manifest.d0dd0fb4634431a72dd2733727346559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "993dc0f72c6587ceb914",
+    "url": "/js/chunk-vendors.993dc0f7.js"
+  },
+  {
+    "revision": "dbb83c17958fbfee6d05",
+    "url": "/js/app.dbb83c17.js"
+  },
+  {
+    "revision": "331cdd5601f4fa8b3bd1ea0443b992d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993dc0f72c6587ceb914",
+    "url": "/css/chunk-vendors.72baa2a0.css"
+  },
+  {
+    "revision": "dbb83c17958fbfee6d05",
+    "url": "/css/app.753d3d5b.css"
+  }
+];
